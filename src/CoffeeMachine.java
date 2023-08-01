52827ea285c89964fdfa903f5e3a800baefd6f76
rochelimit1 <wlsrhaqhddk@gmail.com>
2023-08-01 15:42:38 +0900

    1

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
+    public static void main(String[] args) {
+        String buy = "구매";
+        String fill = "채우기";
+        String take = "가져가기";
+        String exit = "종료";
+
+        System.out.println("커피 머신이 가동되어습니다.");
+        System.out.println();
+        System.out.println("현재 커피머신 상태:");
+        System.out.println("남은 재료와 돈 상태:");
+        System.out.println("물: 400ml");
+        System.out.println("우유: 540ml");
+        System.out.println("원두: 120g");
+        System.out.println("일회용 컵: 9개");
+        System.out.println("돈: 50000원");
+
+
+
+
+
+    }
 }
+
